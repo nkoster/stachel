90758b4dac1219ec0b5fb63306f156d6b9191dda
Niels <n.koster@portavita.eu>
2024-03-10 16:22:55 +0100

    Use random source port + Translate comments to English

--- main.go
+++ main.go
@@ -36,62 +36,62 @@ func main() {
 }
 
 func sendTCPSYN(srcIP string, dstIP net.IP, dstPort uint16) {
-	// Maak een RAW socket
+	// Create raw socket
 	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_TCP)
 	if err != nil {
 		log.Fatalf("Failed to create socket: %v", err)
 	}
 	defer syscall.Close(fd)
 
-	// Stel de IP_HDRINCL optie in zodat we onze eigen IP-header kunnen opgeven
+	// Set the IP_HDRINCL option so that we can specify our own IP header.
 	if err := syscall.SetsockoptInt(fd, syscall.IPPROTO_IP, syscall.IP_HDRINCL, 1); err != nil {
 		log.Fatalf("Failed to set IP_HDRINCL: %v", err)
 	}
 
-	// Bouw het TCP SYN-pakket
+	// Build TCP SYN packet
 	packet := makeTCPSYNPacket(srcIP, dstIP, dstPort)
 
-	// Specificeer de bestemmingsadresstructuur
+	// Specify the destination address structure
 	addr := syscall.SockaddrInet4{
 		Port: int(dstPort),
 		Addr: [4]byte{dstIP[0], dstIP[1], dstIP[2], dstIP[3]},
 	}
 
-	// Verzend het pakket
+	// Send packet
 	if err := syscall.Sendto(fd, packet, 0, &addr); err != nil {
 		log.Fatalf("Failed to send packet: %v", err)
 	}
 }
 
 func makeTCPSYNPacket(srcIP string, dstIP net.IP, dstPort uint16) []byte {
-	// Dit is een zeer basisvoorbeeld en mist vele vereiste stappen voor een volledig functioneel pakket
+
 	var packet []byte
 
-	// Voorbeeld IP-header (vereenvoudigd)
+	// Simplified IP header
 	ipHeader := make([]byte, 20)
-	ipHeader[0] = 0x45                            // Version en IHL
+	ipHeader[0] = 0x45                            // Version and IHL
 	ipHeader[1] = 0x00                            // Type of Service
-	binary.BigEndian.PutUint16(ipHeader[2:4], 40) // Totale lengte
-	binary.BigEndian.PutUint16(ipHeader[4:6], 1)  // Identificatie
-	binary.BigEndian.PutUint16(ipHeader[6:8], 0)  // Flags en Fragment Offset
+	binary.BigEndian.PutUint16(ipHeader[2:4], 40) // Total length
+	binary.BigEndian.PutUint16(ipHeader[4:6], 1)  // Identification
+	binary.BigEndian.PutUint16(ipHeader[6:8], 0)  // Flags and Fragment Offset
 	ipHeader[8] = 64                              // TTL
 	ipHeader[9] = 6                               // Protocol (TCP)
-	// Header checksum wordt later berekend
-	copy(ipHeader[12:16], net.ParseIP(srcIP).To4()) // Bron IP (Vervang dit met een geldig bron-IP)
-	copy(ipHeader[16:20], dstIP.To4())              // Doel IP
+	// Header checksum will be calculated later
+	copy(ipHeader[12:16], net.ParseIP(srcIP).To4()) // Source IP
+	copy(ipHeader[16:20], dstIP.To4())              // Destination IP
 
-	// Voorbeeld TCP-header (vereenvoudigd)
+	// Simplified TCP header
 	tcpHeader := make([]byte, 20)
-	binary.BigEndian.PutUint16(tcpHeader[0:2], 12345)   // Bronpoort
-	binary.BigEndian.PutUint16(tcpHeader[2:4], dstPort) // Doelpoort
-	binary.BigEndian.PutUint32(tcpHeader[4:8], 0)       // Sequence nummer
-	binary.BigEndian.PutUint32(tcpHeader[8:12], 0)      // Acknowledgement nummer
-	tcpHeader[12] = 0x50                                // Data offset en gereserveerd
-	tcpHeader[13] = 0x02                                // Flags (SYN)
-	binary.BigEndian.PutUint16(tcpHeader[14:16], 14600) // Window size
-	// Checksum en urgent pointer worden later berekend
+	binary.BigEndian.PutUint16(tcpHeader[0:2], uint16(rand.Intn(64512)+1024)) // Source port
+	binary.BigEndian.PutUint16(tcpHeader[2:4], dstPort)                       // Target port
+	binary.BigEndian.PutUint32(tcpHeader[4:8], 0)                             // Sequence number
+	binary.BigEndian.PutUint32(tcpHeader[8:12], 0)                            // Acknowledgement number
+	tcpHeader[12] = 0x50                                                      // Data offset and reserved bits
+	tcpHeader[13] = 0x02                                                      // Flags (SYN)
+	binary.BigEndian.PutUint16(tcpHeader[14:16], 14600)                       // Window size
+	// Checksum and urgent pointer will be calculated later
 
-	// Voeg de pseudo-header toe voor checksumberekening
+	// Add the pseudo-header for checksum calculation
 	pseudoHeader := makePseudoHeader(srcIP, dstIP, 6, uint16(len(tcpHeader)))
 	checksumData := append(pseudoHeader, tcpHeader...)
 	binary.BigEndian.PutUint16(tcpHeader[16:18], calculateChecksum(checksumData)) // Checksum
@@ -102,11 +102,11 @@ func makeTCPSYNPacket(srcIP string, dstIP net.IP, dstPort uint16) []byte {
 
 func makePseudoHeader(srcIP string, dstIP net.IP, protocol uint8, tcpLength uint16) []byte {
 	pseudoHeader := make([]byte, 12)
-	copy(pseudoHeader[0:4], net.ParseIP(srcIP).To4())          // Bron IP
-	copy(pseudoHeader[4:8], dstIP.To4())                       // Doel IP
-	pseudoHeader[8] = 0                                        // Gereserveerd
-	pseudoHeader[9] = protocol                                 // Protocol
-	binary.BigEndian.PutUint16(pseudoHeader[10:12], tcpLength) // TCP-lengte
+	copy(pseudoHeader[0:4], net.ParseIP(srcIP).To4()) // Source IP
+	copy(pseudoHeader[4:8], dstIP.To4())              // Target IP
+	pseudoHeader[8] = 0                               // Reserved
+	pseudoHeader[9] = protocol
+	binary.BigEndian.PutUint16(pseudoHeader[10:12], tcpLength) // TCP length
 
 	return pseudoHeader
 }
@@ -117,7 +117,7 @@ func calculateChecksum(data []byte) uint16 {
 		sum += uint32(data[i])<<8 + uint32(data[i+1])
 	}
 	if len(data)%2 == 1 {
-		sum += uint32(data[len(data)-1]) << 8 // Voeg de laatste byte toe als de lengte oneven is
+		sum += uint32(data[len(data)-1]) << 8 // Add last byte if length is odd
 	}
 	sum = (sum >> 16) + (sum & 0xFFFF)
 	sum += sum >> 16
